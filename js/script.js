cc43d978a094593897e3bffca214302b93a4c9d5
sajjadhossainsunny <mdsajjadhossainsunny1@gmail.com>
2023-04-28 13:09:32 +0600

    files update with new setion

---
+++ js/script.js
@@ -0,0 +1,30 @@
+// variables
+let age = document.querySelector(".age_input");
+let job = document.querySelector(".job_input");
+let salary = document.querySelector(".salary_input");
+let button = document.querySelector(".check_btn");
+let result = document.querySelector(".result");
+
+// button event listener
+button.addEventListener("click", function () {
+  if (age.value >= 18 && age.value < 40) {
+    if (job.value.toLowerCase() === "yes") {
+      if (salary.value >= 20000) {
+        result.innerHTML = "Badhai ho bete";
+        result.style.color = "green";
+      } else {
+        result.innerHTML = "Age income barao";
+        result.style.color = "yellow";
+      }
+    } else {
+      result.innerHTML = "MEYER BABA JUTA PITA KORBE";
+      result.style.color = "red";
+    }
+  } else if (age.value >= 40) {
+    result.innerHTML = "Ebar Asha Charo";
+    result.style.color = "purple";
+  } else {
+    result.innerHTML = "Dure ja shala";
+    result.style.color = "red";
+  }
+});
